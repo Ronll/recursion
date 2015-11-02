7314829188e858ecbdff8fce5247bcaca9450031
Ronll <lapushnerron@gmail.com>
2015-11-02 18:28:07 +0200

    Solved the recursion exercise

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+    var result = [];
+    var classNames = className.split(' ');
+
+    checkClass(document);
+
+    function checkClass(obj){
+        var children = obj.children;
+        for(var i = 0; i < children.length ; i++){
+            if( children[i].classList.contains(className)){
+                result.push(children[i]);
+            }
+                checkClass(children[i]);
+        }
+    }
+    return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,64 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+    // your code goes here
+    var result = '';
+
+    switch (typeof obj) {
+        case 'boolean':
+            result += obj
+            break;
+        case 'string':
+            result += '"' + obj + '"'
+            break;
+        case 'number':
+            result += obj
+            break;
+        case 'function':
+            return '';
+            break;
+        case 'symbol':
+            return '';
+            break;
+        case 'undefined':
+            return '';
+            break;
+        case 'object':
+            if (Array.isArray(obj)) {
+                result += '[';
+                if (obj.length !== 0) {
+                    obj.forEach(function (element, index, array) {
+                        result += stringifyJSON(element);
+                        result += ',';
+                    })
+                    //removing last comma
+                    result = result.slice(0, -1);
+                }
+                result += ']';
+            }
+            else if (obj === null) {
+                result += 'null';
+            }
+            else {
+                result += '{';
+                for (var key in obj) {
+                    if (stringifyJSON(obj[key]) == '') {
+                        continue;
+                    }
+                    result += stringifyJSON(key);
+                    result += ':';
+                    result += stringifyJSON(obj[key]);
+                    result += ',';
+                }
+                //removing last comma only if it was added
+                if (result.slice(-1) !== '{') {
+                    result = result.slice(0, -1);
+                }
+                result += '}';
+            }
+            break;
+    }
+
+    return result;
 };
